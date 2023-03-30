a22fea7908dabce2080893fb207e18fd6dc6ed48
Homrai <90481686+Homrai@users.noreply.github.com>
2023-03-29 21:41:08 -0500

    refresh token

--- src/utils/tokenManage.js
+++ src/utils/tokenManage.js
@@ -16,7 +16,7 @@ export const refreshToken = (uid, nombre, res)=>{
     const expiresIn=60*60*24*30;
     try {
         const refreshToken = jwt.sign({uid,nombre}, process.env.JWT_REFRESH, {expiresIn});
-        res.cookie("__session", refreshToken,{
+        res.cookie("refreshToken", refreshToken,{
             httpOnly:true,
             Secure: true,
             expires: new Date(Date.now()+expiresIn*1000),
