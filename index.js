7ec79384f7b2cdc6f0aa85420a7782b24ff698e8
Homrai <90481686+Homrai@users.noreply.github.com>
2023-05-23 17:09:36 -0500

    Update index.js
    quitar cors

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ import cookieParser from "cookie-parser";
 const app = express();
 const PORT = process.env.PORT || 5000;
 const whiteList=[process.env.ORIGIN1,process.env.ORIGIN2,"https://natural-market-front.onrender.com","https://vasudevatiendaonline.shop","https://natural-market.netlify.app","https://tienda-online-vasu.web.app"];
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
-app.use(
-    cors({
-        origin:(origin, callback)=>{
-            if (!origin || whiteList.includes(origin)) {
-                return callback(null, origin)
-            }
-            return callback("Protegido por CORS :"+ origin + "No autorizado!")
-        },
-        credentials:true,
-        optionSuccessStatus:200,
-    })
-);
+//const corsOptions ={
+//    origin:'*', 
+//    credentials:true,            //access-control-allow-credentials:true
+//    optionSuccessStatus:200,
+// }
+//app.use(
+//    cors({
+//        origin:(origin, callback)=>{
+//            if (!origin || whiteList.includes(origin)) {
+//                return callback(null, origin)
+//            }
+//            return callback("Protegido por CORS :"+ origin + "No autorizado!")
+//        },
+//        credentials:true,
+//        optionSuccessStatus:200,
+//    })
+//);
 app.use(cookieParser());
 app.use(express.json({limit: '55mb'}));
 app.use("/auth", authRouter);
